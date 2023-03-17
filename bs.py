0b8d2b6b77622bebce9d37a5bcdbc028998662e3
rabiul <freelancerrabiul24@gmail.com>
2023-03-17 10:03:29 +0600

    git commit -m "last commit"

--- bs.py
+++ bs.py
@@ -1,23 +1,35 @@
-def binary_search(lst, target):
+"""You're going to write a binary search function.
+You should use an iterative approach - meaning
+using loops.
+Your function should take two inputs:
+a Python list to search through, and the value
+you're searching for.
+Assume the list only has distinct elements,
+meaning there are no repeated values, and 
+elements are in a strictly increasing order.
+Return the index of value, or -1 if the value
+doesn't exist in the list."""
 
-    left_index = 0
-    right_index = len(lst) - 1
-    mid_index = 0
 
-    while left_index <= right_index:
-        mid_index = (left_index + right_index) // 2
+def binary_search(input_array, value):
+    """Your code goes here."""
+    left = 0
+    right = len(input_array) - 1
+    mid = 0
 
-        if lst[mid_index] > target:
-            right_index = mid_index - 1
-
-        elif lst[mid_index] < target:
-            left_index = mid_index + 1
+    while left <= right:
+        mid = (left + right) // 2
+        if input_array[mid] > value:
+            right = mid - 1
+        elif input_array[mid] < value:
+            left = mid + 1
         else:
-            return 'Element is found at index ' + str(mid_index)
-    return 'Element does not exist!'
+            return mid
+    return -1
 
 
-lst = list(range(1, 100))
-target = 77
-bs = binary_search(lst, target)
-print(bs)
+test_list = [1, 3, 9, 11, 15, 19, 29]
+test_val1 = 25
+test_val2 = 15
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))
